3383aa9fea3747934590874f3d3a2a866e8d4dd5
MatheusBrasil19 <matheusbrasil862@gmail.com>
2022-11-08 20:13:12 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Atividade 4 Função String</title>
+</head>
+<body>
+    <Form action="atividade4.php" method="post">
+        <h1>Conversor de Data</h1>
+        <br><br>
+        <label for="dt">insira a Data:</label>
+        <br><br>
+        <input type="text" id="dt" name="dt"/>
+        <br><br>
+        <input type="submit" value="Converter">
+    </Form>
+</body>
+</html>
